d599ac14d99ac6645bfc56c8b66be73fd8a8a043
ChangeGod <42135209+ChangeGod@users.noreply.github.com>
2025-04-18 15:03:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { XMLBuilder } from 'fast-xml-parser';
 // Tạo parser với header giả lập trình duyệt
 const parser = new Parser({
   headers: {
-    'User-Agent': 'Mozilla/5.0 (compatible; RSSBot/1.0; +https://example.com/rss)'
+    'User-Agent': 'Inoreader/1.0 (+https://www.inoreader.com)'
   }
 });
 
